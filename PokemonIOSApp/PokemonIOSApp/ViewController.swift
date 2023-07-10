f05b714955aabf5322dd2ecea00ab8689969abc8
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-07-10 23:09:41 +0900

    chore: create work space for UIKit.

---
+++ PokemonIOSApp/PokemonIOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonIOSApp
+//
+//  Created by 松浦裕久 on 2023/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
