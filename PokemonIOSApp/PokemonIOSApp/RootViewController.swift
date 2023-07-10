60dbf88d6dbd0b1ddcd9693658bd67f51cbae943
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-07-10 23:47:08 +0900

    chore: set root view controller.

---
+++ PokemonIOSApp/PokemonIOSApp/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  PokemonIOSApp
+//
+//  Created by 松浦裕久 on 2023/07/10.
+//
+
+import Foundation
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
---
+++ PokemonIOSApp/PokemonIOSApp/ViewController/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  PokemonIOSApp
+//
+//  Created by 松浦裕久 on 2023/07/10.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
